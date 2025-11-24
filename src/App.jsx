32f9594f339c16c2a0c045293fa90b575086124a
Tharunsai1234 <tbhemir2@gitam.in>
2025-11-24 10:33:47 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function TaskManager() {
   );
 
   const { user, logout, token } = useAuth();
-  const API = `${import.meta.env.VITE_API_URL}/tasks` || 'https://task-manager-backend-d3hb.onrender.com/api/tasks';
+  const API = `${import.meta.env.VITE_API_URL}/tasks` || 'https://task-manager-backend-d3hb.onrender.com/';
   
 
   // Dark Mode Effect
